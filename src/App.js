937a210061b71f1bf1f99e61b7387dcf7865d1ec
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-07 15:04:20 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import AuthContainer from './components/AuthComponents/AuthContainer';
 import Feed from './components/HomeComponents/Feed'
 import store from './store'
 import { Provider } from 'react-redux'
+import jwtDecode from 'jwt-decode'
+import { setAuthorizationToken, setCurrentUser } from './actions/auth';
+
+if(localStorage.jwtToken){
+  setAuthorizationToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch {
+    store.dispatch(setCurrentUser({}));
+  }
+}
 
 function App() {
   return (
